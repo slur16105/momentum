4274f7f3d1fc929da8960d90fcae3ea7bba30b04
hoseong <43123909+hoseong85@users.noreply.github.com>
2023-02-21 22:37:15 +0900

    #7 TO DO LIST - 0 Setup

---
+++ js/todo.js
@@ -0,0 +1,11 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newTodo = toDoInput.value; // input의 현재 value를 새로운 변수에 복사
+    toDoInput.value = "";
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
