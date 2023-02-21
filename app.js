e1506610e399d2fad1a2574172f922cce3b214e2
hoseong <43123909+hoseong85@users.noreply.github.com>
2023-02-21 18:24:09 +0900

    #4.3 Events part Two

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const loginForm = document.querySelector("#login-form");
 const loginButton = document.querySelector("#login-form button");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
-    event.preventDefault();
+    event.preventDefault(); // sumit의 새로고침되는 기본 기능을 막기 위해 사용
     console.log(loginButton.value);
 }
 
+// a링크의 기본 이벤트 로그로 확인
+// preventDefault를 이해하기
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.log(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
